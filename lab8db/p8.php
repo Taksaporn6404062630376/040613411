ad7e09626f412b37a85a5374a3d07e552861161d
Taksaporn6404062630376 <s6404062630376@email.kmutnb.ac.th>
2023-09-23 06:43:53 +0700

    workshop1-3

---
+++ lab8db/p8.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+            $message = "สวัสดี<br>";
+            echo $message;
+            $myname = 'aws';
+            $age=43;
+        ?>
+        ชื่อ:<?=$myname?><br>
+        อายุ:<?=$age?>
+</body>
+</html>
